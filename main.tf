a4296b413b5fcce2f67df4950299ecee823186b8
Rich Johnson <48325769+johnsonrt63@users.noreply.github.com>
2026-09-07 15:33:31 -0400

    create security groups

--- main.tf
+++ main.tf
@@ -14,11 +14,56 @@ data "aws_ami" "app_ami" {
   owners = ["099720109477"]
 }
 
+data "aws_vpc" "default" {
+  default= true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.websec.id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_security_group" "websec" {
+  name        = "websec"
+  description = "Allow HTTP[s] in, everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.websec.id
+}
+
+
+resource "aws_security_group_rule" "web_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.websec.id
+}
+
+resource "aws_security_group_rule" "web_everything_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.websec.id
+}
+
